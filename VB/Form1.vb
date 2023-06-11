a7cbbf5ca5f6247230f441b973fee016a0bec497
DevExpressExampleBot <fakeEmail>
2023-06-11 21:35:51 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,59 +1,54 @@
-﻿Imports Microsoft.VisualBasic
-#Region "#usings"
+'#Region "#usings"
 Imports System
 Imports System.Windows.Forms
 Imports System.Diagnostics
 Imports DevExpress.XtraPrinting
-Imports DevExpress.XtraReports.UI
+
 ' ...
-#End Region ' #usings
-
+'#End Region  ' #usings
 Namespace ExportToXlsCS
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		#Region "#export"
-		Private Sub ExportToXLS()
-			' A path to export a report.
-			Dim reportPath As String = "c:\Test.xls"
+    Public Partial Class Form1
+        Inherits Form
 
-			' Create a report instance.
-			Dim report As New XtraReport1()
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Get its XLS export options.
-			Dim xlsOptions As XlsExportOptions = report.ExportOptions.Xls
+'#Region "#export"
+        Private Sub ExportToXLS()
+            ' A path to export a report.
+            Dim reportPath As String = "c:\Test.xls"
+            ' Create a report instance.
+            Dim report As XtraReport1 = New XtraReport1()
+            ' Get its XLS export options.
+            Dim xlsOptions As XlsExportOptions = report.ExportOptions.Xls
+            ' Set XLS-specific export options.
+            xlsOptions.ShowGridLines = True
+            xlsOptions.TextExportMode = TextExportMode.Value
+            ' Export the report to XLS.
+            report.ExportToXls(reportPath)
+            ' Show the result.
+            StartProcess(reportPath)
+        End Sub
 
-			' Set XLS-specific export options.
-			xlsOptions.ShowGridLines = True
-			xlsOptions.TextExportMode = TextExportMode.Value
+'#End Region  ' #export
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ExportToXLS()
+        End Sub
 
-			' Export the report to XLS.
-			report.ExportToXls(reportPath)
-
-			' Show the result.
-			StartProcess(reportPath)
-		End Sub
-		#End Region ' #export
-
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			ExportToXLS()
-		End Sub
-
-		#Region "#startprocess"
-		' Use this method if you want to automaically open
-		' the created XLS file in the default program.
-		Public Sub StartProcess(ByVal path As String)
-			Dim process As New Process()
-			Try
-				process.StartInfo.FileName = path
-				process.Start()
-				process.WaitForInputIdle()
-			Catch
-			End Try
-		End Sub
-		#End Region ' #startprocess
-	End Class
+'#Region "#startprocess"
+        ' Use this method if you want to automaically open
+        ' the created XLS file in the default program.
+        Public Sub StartProcess(ByVal path As String)
+            Dim process As Process = New Process()
+            Try
+                process.StartInfo.FileName = path
+                process.Start()
+                process.WaitForInputIdle()
+            Catch
+            End Try
+        End Sub
+'#End Region  ' #startprocess
+    End Class
 End Namespace
--- VB/XtraReport1.vb
+++ VB/XtraReport1.vb
@@ -1,11 +1,10 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace ExportToXlsCS
-	Partial Public Class XtraReport1
-		Inherits DevExpress.XtraReports.UI.XtraReport
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-	End Class
+    Public Partial Class XtraReport1
+        Inherits DevExpress.XtraReports.UI.XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
